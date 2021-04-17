96633c69be199d2716697afba754ed599971f6b0
matteo-d <matteodrouin33@gmail.com>
2021-04-18 01:33:25 +0200

    display product ok, début local storage

--- frontend/produit.js
+++ frontend/produit.js
@@ -1,41 +1,72 @@
 /* Récupération de l'id du produit sélectionné dans la page précédente */
-const productId = window.location.search.substr(1); 
+const productId = window.location.search.substr(1);
 
-/* Récupération du produit avec l'id associé depuis le serveur */ 
+/* Récupération du produit avec l'id associé depuis le serveur */
 
 fetch(`http://localhost:3000/api/teddies/${productId}`)
-    .then((productResp) => productResp.json())
-    .then(productResp => {
-        
-    let html="";
-    // Affichage du produit / personalisation
-    html +=  `<div class="product">
+  .then((productResp) => productResp.json())
+  .then((productResp) => {
+    let html = "";
+    // Affichage du produit 
+    html = `<div class="product">
     <img src="${productResp.imageUrl}" alt="Ours ${productResp.name}">
     <article class="product_infos">
           <h2 id="name_product_${productResp.name}">${productResp.name}</h2>
-          <p class="product_description" id="description_product_${productResp.name}">${productResp.description}</p>   
-          <p class="price" id="price_product_${productResp.name}">${(productResp.price/100).toFixed(2).replace(".",",")} €</p>
+          <p class="product_description" id="description_product_${
+            productResp.name}">${productResp.description}</p>   
+          <p class="price" id="price_product_${productResp.name}">${(productResp.price / 100).toFixed(2).replace(".", ",")} €</p>
         <!-- Personalisation de la couleur -->
         <label for="select__color">
             <h3>Personnaliser votre ours</h3>
         </label>
-            <select class="section_colors" name="colors" id="select__color">
-            </select>
-        <button id="btn" class="addCart">Add</button>
-        </div>`
+        <select class="section_choice" name="colors" id="select_choice">
+        <!-- Mes choix de couleurs dans la function forEach --!>
+        </select>
+            <button class="addCart">Add</button>
+        </div>`;
     document.getElementById("main").innerHTML = html;
-    
-    //Création d'une function foreach pour afficher mes choix de couleurs
-    let choice = document.querySelector(".section_colors");
-    
-    productResp.colors.forEach (function (color) {
-        let option = document.createElement("option");
-        option.value = color;
-        option.textContent = color;
-        choice.appendChild(option);
-    })
 
-         
+    //Affichage des choix
+    let choice = document.querySelector(".section_choice");
 
-    })
+    productResp.colors.forEach(function (colors) {
+      let option = document.createElement("option");
+      option.value = colors;
+      option.textContent = colors;
+      choice.appendChild(option);
+    });
+   
+    // Au clic l'élément et otption sélectionné au local storage
+let btnAddToCart = document.querySelector(".addCart");
+let selectedValue = document.getElementById("select_choice");
+  // Array qui contiendra les Cart Items 
+btnAddToCart.addEventListener('click', function(e)  {
+    e.preventDefault();
+    // trouver la valeur selectionée par l'utilisateur
+    let selectedColor = selectedValue.options[selectedValue.selectedIndex].value;
+    
+    // stockage de la valeur sélectionné dans un objet
+    let cartItem = {
+        _id: productResp._id,
+        imageUrl: productResp.imageUrl,
+        name: productResp.name,
+        price: productResp.price,
+        quantity: 1,
+        selectColors: selectedColor
+    }
+    // Local Storage  
+ 
+   // Ajoute le produit choisi au panier ( local storage )
+   localStorage.setItem('item', JSON.stringify(cartItem));
   
+ 
+})
+
+
+
+
+
+
+
+
+  });
