76595394545eb645a3c1ad9713cb4871597234ea
matteo-d <matteodrouin33@gmail.com>
2021-04-19 01:09:07 +0200

    quantité choisis ajouter à l'objet stocké dans le local storage

--- frontend/produit.js
+++ frontend/produit.js
@@ -11,19 +11,15 @@ fetch(`http://localhost:3000/api/teddies/${productId}`)
     html = `<div class="product">
     <img src="${productResp.imageUrl}" alt="Ours ${productResp.name}">
     <article class="product_infos">
-          <h2 id="name_product_${productResp.name}">${productResp.name}</h2>
-          <p class="product_description" id="description_product_${
-            productResp.name}">${productResp.description}</p>   
-          <p class="price" id="price_product_${productResp.name}">${(productResp.price / 100).toFixed(2).replace(".", ",")} €</p>
-     
-          <!-- Personalisation de la couleur -->
+      <h2 id="name_product_${productResp.name}">${productResp.name}</h2>
+        <p class="product_description" id="description_product_${productResp.name}">${productResp.description}</p>   
+        <p class="price" id="price_product_${productResp.name}">${(productResp.price / 100).toFixed(2).replace(".", ",")} €</p>    
         <label for="select__color">
             <h3>Personnaliser votre ours</h3>
         </label>
         <select class="section_choice" name="colors" id="select_choice">
-        <!-- Mes choix de couleurs dans la function forEach --!>
-        </select> 
-        
+          <!-- Mes choix de couleurs dans la function forEach --!>
+        </select>        
         <!-- Personalisation de la quantité -->
         <div id="container_quantity">
             <button id="btnMinus">-</button>
@@ -34,8 +30,8 @@ fetch(`http://localhost:3000/api/teddies/${productId}`)
         <div id="container_addToCart">
             <button id="addToCart" class="addCart">Add</button>
         </div>`;
-
     document.getElementById("main").innerHTML = html;
+    
     //Affichage des choix de couleurs
     let choice = document.querySelector(".section_choice");
 
@@ -45,6 +41,7 @@ fetch(`http://localhost:3000/api/teddies/${productId}`)
       option.textContent = colors;
       choice.appendChild(option);
     });
+
    // Logique boutons quantité + et - 
     let btnPlus = document.getElementById('btnPlus');
     let btnMinus = document.getElementById('btnMinus');
@@ -58,24 +55,26 @@ fetch(`http://localhost:3000/api/teddies/${productId}`)
     btnMinus.addEventListener('click', function() {
       compteur --;
         quantity.innerHTML = compteur;
-  });
- // Au clic l'élément et otption sélectionné au local storage
-let btnAddToCart = document.querySelector(".addCart");
-let selectedValue = document.getElementById("select_choice");
-  // Array qui contiendra les Cart Items 
-btnAddToCart.addEventListener('click', function(e)  {
+    });
+    // Au clic l'élément et otption sélectionné au local storage
+    let btnAddToCart = document.querySelector(".addCart");
+    let selectedValue = document.getElementById("select_choice");
+    // Array qui contiendra les Cart Items 
+    btnAddToCart.addEventListener('click', function(e)  {
     e.preventDefault();
     // trouver la valeur selectionée par l'utilisateur
     let selectedColor = selectedValue.options[selectedValue.selectedIndex].value;
     
+    let chosenQuantity = parseInt(quantity.innerText);
+    console.log(chosenQuantity)
     // stockage de la valeur sélectionné dans un objet
     let cartItem = {
-        _id: productResp._id,
-        imageUrl: productResp.imageUrl,
-        name: productResp.name,
-        price: productResp.price,
-        quantity: 1,
-        selectColors: selectedColor
+      _id: productResp._id,
+      imageUrl: productResp.imageUrl,
+      name: productResp.name,
+      price: productResp.price,
+      chosenQuantity: chosenQuantity,
+      selectColors: selectedColor,
     }
     // Local Storage  
     // Si Local storage vide productInLocalStorage = null
@@ -87,11 +86,11 @@ btnAddToCart.addEventListener('click', function(e)  {
      localStorage.setItem("cartItem", JSON.stringify(productInLocalStorage));
     }
     else {
-      console.log(' Pas articles dans le panier')
-      productInLocalStorage = []
-      productInLocalStorage.push(cartItem); 
-      localStorage.setItem("cartItem", JSON.stringify(productInLocalStorage));
-      console.log(productInLocalStorage);
+    console.log(' Pas articles dans le panier')
+    productInLocalStorage = []
+    productInLocalStorage.push(cartItem); 
+    localStorage.setItem("cartItem", JSON.stringify(productInLocalStorage));
+    console.log(productInLocalStorage);
     }
 });
 
